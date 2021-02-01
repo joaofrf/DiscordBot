c4b6319d838bfef82d16706f0c81b291636c7e9d
João Ferreira <2171315@my.ipleiria.pt>
2021-02-01 22:10:39 +0000

    Comments added

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
+// REQUIRES
 const Discord = require("discord.js");
 const config = require("./config.json");
+const ytdl = require('ytdl-core');
 
+// Bot settings
 const client = new Discord.Client();
-
 client.login(config.BOT_TOKEN);
-
-
 const prefix = config.PREFIX;
+const queue = new Map(); // music queue
+
 
 client.once('ready', () => {
 	console.log('Bot Started With Success!');
 	console.log('_________________________');
 });
 
-client.on("message", function(message) {
+client.once('reconnecting', () => {
+ console.log('Bot is reconnecting...');
+});
 
-  if (message.author.bot || !message.content.startsWith(prefix)){
-  	return;
-  }
+client.once('disconnect', () => {
+ console.log('Bot disconnected...');
+});
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args[0].toLowerCase();
+client.on("message", async message => {
 
-	/*
-  console.log("body:");
-  console.log(commandBody); //say balshit monte
-  console.log("args:");
-  console.log(args); // [ 'say', 'balshit', 'monte' ]
-  console.log("command:");
-  console.log(command); // say
-	*/
-
-  if (command === "say") {
-    var i;
-    let saymsg = "";
-    for (i = 1; i < args.length; i++) {
-    	saymsg+= args[i] + " ";
+    //if message is from bot or message does not start with prefix -> ignore
+    if (message.author.bot || !message.content.startsWith(prefix)){
+        return;
     }
 
-    message.channel.send(saymsg);
-  }
+    //explanation bellow
+    const commandBody = message.content.slice(prefix.length); 
+    const args = commandBody.split(' ');
+    const command = args[0].toLowerCase();
+    const serverQueue = queue.get(message.guild.id);
 
-  
+	/*
+      console.log("body:");
+      console.log(commandBody); //say balshit monte
+      console.log("args:");
+      console.log(args); // [ 'say', 'balshit', 'monte' ]
+      console.log("command:");
+      console.log(command); // say
+	*/
 
+    //Commands inside Switch statement with { } for better reading.
+    switch(command) {
+        case "say":{ //Say command. Says whatever was said after "prefix say"
+            var i;
 
+            let saymsg = "";
+
+            for (i = 1; i < args.length; i++) {
+               saymsg += args[i] + " ";
+            }
+
+            message.channel.send(saymsg);
+
+            break;
+        }
+        case "play":{ //play command. adds to queue the music, if found.
+
+            break;
+        }
+        default:{
+            message.channel.send("Wtf is that command u stupid?");
+        }
+        
+    }
 
 
 
